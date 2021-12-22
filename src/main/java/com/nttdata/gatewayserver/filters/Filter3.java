5db1bb22e92854a253e6b108035ed5894331208d
AlinConstantinVatamanu <alin.vatamanu7@gmail.com>
2021-12-22 15:56:19 +0100

    Spring Cloud - segundo reto

---
+++ src/main/java/com/nttdata/gatewayserver/filters/Filter3.java
@@ -0,0 +1,29 @@
+package com.nttdata.gatewayserver.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class Filter3 implements GlobalFilter, Ordered {
+    
+	Logger logger = LoggerFactory.getLogger(Filter3.class);
+    
+	@Override
+	public int getOrder() {
+        return 3;
+    }
+	
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("third pre filter");
+        return chain.filter(exchange).then(Mono.fromRunnable(() ->        
+        logger.info("third post filter")
+        ));
+    }
+}
